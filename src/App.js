f1a6ca3b050c796aaaa89f268ddcc8d06acba6ff
Your Name <prafulsanal@gmail.com>
2025-07-24 13:10:45 +0530

    first change2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './Components/LoginPage';
+import DoctorDashboard from './Components/DoctorDashboard';
+import ReceptionistDashboard from './Components/ReceptionistDashboard';
 import './App.css';
-
+  
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/doctor" element={<DoctorDashboard />} />
+        <Route path="/receptionist" element={<ReceptionistDashboard />} />
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
