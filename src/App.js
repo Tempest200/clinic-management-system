8a73f8b0acedecac3425e890c56376dfd1e75c58
Your Name <prafulsanal@gmail.com>
2025-07-24 14:22:06 +0530

    new

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ import './App.css';
   
 function App() {
   return (
-    <Router>
+   <Router basename="/clinic-management-system"> {/* basename is important for GitHub Pages */}
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/doctor" element={<DoctorDashboard />} />
         <Route path="/receptionist" element={<ReceptionistDashboard />} />
+        {/* Fallback route */}
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </Router>
   );
 }
-
 
 export default App;
